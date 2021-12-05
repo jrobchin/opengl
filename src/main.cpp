08c6902b71448e1389f4d79f343a9d9b022945cc
Jason Chin <jason.rob.chin@gmail.com>
2021-12-04 22:02:27 -0500

    add exercise map

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
 #include <string.h>
+#include <memory>
+#include <map>
 #include <iostream>
 #include <getopt.h>
 
 #include <utils.hpp>
+#include <exercise.hpp>
 #include <triangle.hpp>
 
 int main(int argc, char *const argv[])
 {
+  std::map<std::string, Exercise *> m{
+      {"triangle", new Triangle()}};
+
+  Exercise *exercise;
+
   for (;;)
   {
     switch (getopt(argc, argv, "p:")) // note the colon (:) to indicate that 'b' has a parameter and is not a switch
     {
     case 'p':
-      if (strcmp(optarg, "triangle") == 0)
-      {
-        std::cout << "Running triangle" << std::endl;
-        Triangle().initialize().run();
-      }
+      exercise = m[optarg];
+      exercise->initialize().run();
       break;
     case -1:
       break;
