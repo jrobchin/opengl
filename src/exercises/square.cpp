f8f5524ad476686db0ead4e88099ac29cc808444
Jason Chin <jason.rob.chin@gmail.com>
2021-12-05 21:14:42 -0500

    remove commented line

--- src/exercises/square.cpp
+++ src/exercises/square.cpp
@@ -54,7 +54,6 @@ Square &Square::run()
         glUseProgram(basicShaderProgram);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
